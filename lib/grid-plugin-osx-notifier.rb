ac2102e5489ecc6f441133326845419e69ca7277
Brian Muller <brian.muller@livingsocial.com>
2012-10-05 20:02:21 -0400

    fixed issue when there are no posts

--- lib/grid-plugin-osx-notifier.rb
+++ lib/grid-plugin-osx-notifier.rb
@@ -4,30 +4,31 @@ require 'terminal-notifier'
 module Grid
   module Plugin
     module OSXNotifier
-      VERSION = "0.0.1"
+      VERSION = "0.0.2"
     end
   end
 end
 
 GridCLI.hooker.register(:after_update) { |posts|
-  return posts if posts.length == 0
+  if posts.length > 0
+    title = "The Grid"
+    if posts.length == 1
+      subtitle = "You have a new message."
+    else
+      subtitle = "You have #{posts.length} new messages."
+    end
 
-  title = "The Grid"
-  if posts.length == 1
-    subtitle = "You have a new message."
-  else
-    subtitle = "You have #{posts.length} new messages."
+    senders = posts.map { |p| 
+      p.send(p.known_attributes.first).from_username
+    }.uniq
+
+    if senders.length > 3
+      senders = senders.slice(0, 3) + ["and others"]
+    end
+
+    msg = "From " + senders.join(", ")
+    TerminalNotifier.notify(msg, :title => title, :subtitle => subtitle)
   end
 
-  senders = posts.map { |p| 
-    p.send(p.known_attributes.first).from_username
-  }.uniq
-
-  if senders.length > 3
-    senders = senders.slice(0, 3) + ["and others"]
-  end
-
-  msg = "From " + senders.join(", ")
-  TerminalNotifier.notify(msg, :title => title, :subtitle => subtitle)
   posts
 }
